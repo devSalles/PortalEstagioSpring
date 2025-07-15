c84ac2169e4685d1dc3f2f88c7d081d42276ff46
Bernardo de Salles Cruz <168240480+devSalles@users.noreply.github.com>
2025-07-15 15:20:21 -0300

    Add files via upload

---
+++ VagaEstagio/VagaEstagio/src/main/java/VagaEstagio/VagaEstagioApplication.java
@@ -0,0 +1,13 @@
+package VagaEstagio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagaEstagioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagaEstagioApplication.class, args);
+	}
+
+}
---
+++ VagaEstagio/VagaEstagio/src/test/java/VagaEstagio/VagaEstagioApplicationTests.java
@@ -0,0 +1,13 @@
+package VagaEstagio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagaEstagioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
