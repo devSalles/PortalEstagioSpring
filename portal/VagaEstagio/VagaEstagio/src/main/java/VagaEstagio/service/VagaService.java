314ba68aa16e0c665f2abc87f7a4c8756597e55d
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2025-07-25 19:49:54 -0300

    Refatoraação e conclusão do CRUD de vaga

--- portal/VagaEstagio/VagaEstagio/src/main/java/VagaEstagio/service/VagaService.java
+++ portal/VagaEstagio/VagaEstagio/src/main/java/VagaEstagio/service/VagaService.java
@@ -1,7 +1,9 @@
 package VagaEstagio.service;
 
+import VagaEstagio.core.exception.EmptyListException;
 import VagaEstagio.core.exception.IdNotFoundException;
-import VagaEstagio.dto.VagaDTO;
+import VagaEstagio.dto.vaga.VagaDTO;
+import VagaEstagio.dto.vaga.VagaResponseDTO;
 import VagaEstagio.model.EmpresaModel;
 import VagaEstagio.model.EstagiarioModel;
 import VagaEstagio.model.VagaModel;
@@ -11,6 +13,7 @@ import VagaEstagio.repository.VagaRepository;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -41,9 +44,10 @@ public class VagaService {
 
         if(vagaDTO.getBolsa() == null )
         {
-            throw new IllegalArgumentException("Campo vaga inválido");
+            throw new IllegalArgumentException("Campo bolsa inválido");
         }
 
+        //Verifição de campo valor valido
         if(vagaDTO.getBolsa().compareTo(BigDecimal.ZERO)<=0)
         {
             throw new IllegalArgumentException("Valor deve ser maior que 0");
@@ -54,38 +58,146 @@ public class VagaService {
         {
             throw new IllegalArgumentException("Campo ID de estagiario não pode ser nulo");
         }
-
         //Procura de ID de estagiário
-        Optional<EstagiarioModel>estagiarioID=this.estagiarioRepository.findById(vagaDTO.getEstagiarioModel().getId());
-        if(estagiarioID.isEmpty())
-        {
-            throw new IdNotFoundException();
-        }
+        EstagiarioModel estagiarioID=this.estagiarioRepository.findById(vagaDTO.getEstagiarioModel().getId()).orElseThrow(() -> new IdNotFoundException("ID de estagiario não encontrado"));
+
 
         //Verificação de campo nulo ID de empresa
         if(vagaDTO.getEmpresaModel() == null || vagaDTO.getEmpresaModel().getId() == null)
         {
-            throw new IllegalArgumentException("Campo ID de empresa não pode ser nulo");
+            throw new IllegalArgumentException("Campo ID da empresa não pode ser nulo");
         }
-
         //Procura de ID de empresa
-        Optional<EmpresaModel>empresaID=this.empresaRepository.findById(vagaDTO.getEmpresaModel().getId());
-        if(empresaID.isEmpty())
-        {
-            throw new IdNotFoundException();
-        }
+        EmpresaModel empresaID=this.empresaRepository.findById(vagaDTO.getEmpresaModel().getId()).orElseThrow(() -> new IdNotFoundException("ID de empresa não encontrado"));
+
+        //Settar os ID correspondentes
+        vagaDTO.setEstagiarioModel(estagiarioID);
+        vagaDTO.setEmpresaModel(empresaID);
 
         VagaModel vagaSave=vagaDTO.toVaga();
 
-        //Settar os ID correspondentes
-        vagaDTO.setEstagiarioModel(estagiarioID.get());
-        vagaDTO.setEmpresaModel(empresaID.get());
 
         return this.vagaRepository.save(vagaSave);
     }
 
+    public VagaModel updateById(Long id, VagaDTO vagaDTO)
+    {
+        Optional<VagaModel>vagaID = this.vagaRepository.findById(id);
+        if(vagaID.isEmpty())
+        {
+            throw new IdNotFoundException();
+        }
+
+        if(vagaDTO.getVaga() == null)
+        {
+            throw new IllegalArgumentException("Campo vaga inválido");
+        }
+
+        if(vagaDTO.getBolsa() == null )
+        {
+            throw new IllegalArgumentException("Campo bolsa inválido");
+        }
+
+        //Verifição de campo valor valido
+        if(vagaDTO.getBolsa().compareTo(BigDecimal.ZERO)<=0)
+        {
+            throw new IllegalArgumentException("Valor deve ser maior que 0");
+        }
+
+        //Verifição de campo nulo de ID de estagiário
+        if(vagaDTO.getEstagiarioModel() == null || vagaDTO.getEstagiarioModel().getId() == null)
+        {
+            throw new IllegalArgumentException("Campo ID de estagiario obrigatorio");
+        }
+        Optional<EstagiarioModel>estagiarioID = this.estagiarioRepository.findById(vagaDTO.getEstagiarioModel().getId());
+        if(estagiarioID.isEmpty())
+        {
+            throw new IdNotFoundException("ID de estagiario não encontrado");
+        }
+
+        //Verifição de campo nulo de ID de empresa
+        if(vagaDTO.getEmpresaModel() == null || vagaDTO.getEmpresaModel().getId() == null)
+        {
+            throw new IllegalArgumentException("Campo ID da empresa obrigatorio");
+        }
+        Optional<EmpresaModel>empresaID = this.empresaRepository.findById(vagaDTO.getEmpresaModel().getId());
+        if(empresaID.isEmpty())
+        {
+            throw new IdNotFoundException("ID de empresa não encontrado");
+        }
+
+        VagaModel vagaUpdate=vagaDTO.updateVaga(vagaID.get());
+        vagaDTO.setEmpresaModel(empresaID.get());
+        vagaDTO.setEstagiarioModel(estagiarioID.get());
+
+        this.vagaRepository.save(vagaUpdate);
+        return  vagaUpdate;
+    }
+
+    public List<VagaResponseDTO> getAll()
+    {
+        List<VagaModel>vaga=this.vagaRepository.findAll();
+        if(vaga.isEmpty())
+        {
+            throw new EmptyListException();
+        }
+
+        return vaga.stream().map(VagaResponseDTO::fromVaga).toList();
+    }
+
+    public VagaResponseDTO getById(Long id)
+    {
+        VagaModel vaga =this.vagaRepository.findById(id).orElseThrow(() -> new IdNotFoundException());
+        return VagaResponseDTO.fromVaga(vaga);
+    }
+
+    public Boolean deleteById(Long id)
+    {
+        VagaModel vagaModel = this.vagaRepository.findById(id).orElseThrow(IdNotFoundException::new);
+
+        List<EstagiarioModel>estagiarioID=this.estagiarioRepository.findByVagaModel_Id(id);
+        for (EstagiarioModel estagiario:estagiarioID)
+        {
+            estagiario.setVagaModel(null);
+        }
+        this.estagiarioRepository.saveAll(estagiarioID);
+
+        List<EmpresaModel>empresaID=this.empresaRepository.findByVagaModel_Id(id);
+        for (EmpresaModel model:empresaID)
+        {
+            model.setVagaModel(null);
+        }
+        this.empresaRepository.saveAll(empresaID);
+
+        this.vagaRepository.delete(vagaModel);
+        return true;
+
+    }
+
     public void deleteAll()
     {
+        List<VagaModel>vagaDel=this.vagaRepository.findAll();
+        if(vagaDel.isEmpty())
+        {
+            throw new EmptyListException();
+        }
+        for (VagaModel vagaModel:vagaDel)
+        {
+            List<EmpresaModel>empresa=this.empresaRepository.findByVagaModel_Id(vagaModel.getId());
+            for(EmpresaModel empresaModel:empresa)
+            {
+                empresaModel.setVagaModel(null);
+            }
+            this.empresaRepository.saveAll(empresa);
+
+            List<EstagiarioModel>estagiario=this.estagiarioRepository.findByVagaModel_Id(vagaModel.getId());
+            for(EstagiarioModel estagiarioModel:estagiario)
+            {
+                estagiarioModel.setVagaModel(null);
+            }
+            this.estagiarioRepository.saveAll(estagiario);
+        }
+
         this.vagaRepository.deleteAll();
     }
 }
