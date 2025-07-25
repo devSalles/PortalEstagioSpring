a35c35e4cdee2006bb28bf332d2249ff32197c18
Bernardo de Salles Cruz <sallesbernardo89@gmail.com>
2025-07-25 19:47:07 -0300

    Adicionado novos DTO para exibição de informações

--- portal/VagaEstagio/VagaEstagio/src/main/java/VagaEstagio/dto/empresa/EmpresaResponseDTO.java
+++ portal/VagaEstagio/VagaEstagio/src/main/java/VagaEstagio/dto/empresa/EmpresaResponseDTO.java
@@ -3,12 +3,13 @@ package VagaEstagio.dto.empresa;
 import VagaEstagio.model.EmpresaModel;
 
 public record EmpresaResponseDTO(
+        Long id,
         String nome,
         String cnpj,
         String area
 ) {
     public static EmpresaResponseDTO fromEmpresaResponseDTO(EmpresaModel model)
     {
-        return new EmpresaResponseDTO(model.getNome(), model.getCnpj(), model.getArea());
+        return new EmpresaResponseDTO(model.getId(),model.getNome(), model.getCnpj(), model.getArea());
     }
 }
--- portal/VagaEstagio/VagaEstagio/src/main/java/VagaEstagio/dto/estagiario/EstagiarioResponseDTO.java
+++ portal/VagaEstagio/VagaEstagio/src/main/java/VagaEstagio/dto/estagiario/EstagiarioResponseDTO.java
@@ -3,12 +3,13 @@ package VagaEstagio.dto.estagiario;
 import VagaEstagio.model.EstagiarioModel;
 
 public record EstagiarioResponseDTO(
+        Long id,
         String nome,
         String curso,
         Integer periodo
 ){
     public static EstagiarioResponseDTO fromEstagiarioResponseDTO(EstagiarioModel model)
     {
-        return new EstagiarioResponseDTO(model.getNome(), model.getCurso(), model.getPeriodo());
+        return new EstagiarioResponseDTO(model.getId(),model.getNome(), model.getCurso(), model.getPeriodo());
     }
 }
